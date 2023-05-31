907d958cebaae5aa9fc74a905a20e3d4a15eedc4
Na <kentcandoit89@gmail.com>
2023-05-31 17:59:46 +0900

    어레이 리스트 스캐너

--- DYJavaStudy/src/cp46_ioScanner/crudOperation/Manager.java
+++ DYJavaStudy/src/cp46_ioScanner/crudOperation/Manager.java
@@ -1,73 +1,22 @@
 package cp46_ioScanner.crudOperation;
 
+import java.util.List;
 import java.util.Scanner;
+
+import cp44_collectionList.bookList.Book;
 
 public class Manager {
 	
-	private String id;
-	private String name;
-	private int age;
-	private String sex;
+	private List<Person> personInfo;
 	
-	public Manager() {};
-	
-	
-	public Manager(String id, String name, int age, String sex) {
-		this.id = id;
-		this.name = name;
-		this.age = age;
-		this.sex = sex;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public void setSex(String sex) {
-
+	public Manager() {
 		
-		this.sex = sex;
-	}
+	};
 	
-	public void printInfo() {
-		System.out.println(getId());
-		System.out.println(getName());
-		System.out.println(getAge());
-		System.out.println(getSex());	
-		}
 	
-	public void printPersonList() {
 
-		System.out.println(getId());
-		System.out.println(getName());
-		System.out.println(getAge());
-		System.out.println(getSex());
-		
-	}
+	
+
 	
 	
 	
@@ -89,5 +38,20 @@ public class Manager {
 		System.out.println(getSex());
 		
 	}
+//	String id = sc.nextLine();
+//	String name = sc.nextLine();
+//	String age = sc.nextLine();
+//	String sex = sc.nextLine();
+//	
+//	setId(id);
+//	setName(name);
+//	setAge(Integer.parseInt(age));
+//	setSex(sex);
+//	
+//	
+//	System.out.println("1.아이디="+getId());
+//	System.out.println(" | 이름="+ getName());
+//	System.out.println(" | 나이="+ Integer.toString(getAge()));
+//	System.out.println(getSex());
 
 }
--- DYJavaStudy/src/cp46_ioScanner/crudOperation/Person.java
+++ DYJavaStudy/src/cp46_ioScanner/crudOperation/Person.java
@@ -1,5 +1,50 @@
 package cp46_ioScanner.crudOperation;
 
 public class Person {
+	
+	public Person(){
+		
+	}
+	
+	private String id;
+	private String name;
+	private int age;
+	private String sex;
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Person(String id, String name, int age, String sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+	
+	
+	
+	
+	
 
 }
